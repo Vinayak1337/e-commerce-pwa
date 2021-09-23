d71bd321d3d6fb615547b1682f35ea6cd44a6ddd
Vinayak1337 <vinayak111kumar@gmail.com>
2021-09-24 00:00:01 +0530

    chaging if statement to development

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -6,7 +6,7 @@ import { persistCombined } from './RootReducer';
 
 const middlewares = [];
 
-if (process.env.NODE_ENV === 'production') middlewares.push(logger);
+if (process.env.NODE_ENV === 'development') middlewares.push(logger);
 
 export const Store = createStore(
 	persistCombined,
