f2f71e272d5ef2d734580b35ddf741ceb92f66a9
Vinayak1337 <vinayak111kumar@gmail.com>
2021-09-23 23:56:43 +0530

    changing behaviour for production env

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -4,11 +4,13 @@ import { persistStore } from 'redux-persist';
 
 import { persistCombined } from './RootReducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'production') middlewares.push(logger);
 
 export const Store = createStore(
 	persistCombined,
-	applyMiddleware(...middlewares),
+	applyMiddleware(...middlewares)
 );
 
 export const persistor = persistStore(Store);
